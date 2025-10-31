d7b2d13b4a60c14c239de206f76f499da0ec649d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-10-30 21:39:31 -0600

    feat: Agregando AuthenticateController

---
+++ PortfolioAPI/Controllers/AuthenticateController.cs
@@ -0,0 +1,11 @@
+ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+    }
+}
